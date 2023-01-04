a97187e19a15906f3e9e630539c8ccf4ff4bcb94
Lachlan Paul <lachlanpaul03@gmail.com>
2023-01-04 15:25:11 +1030

    Added date and time to messages and some other stuff. TODO: Fix whiteout after message sent error.

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,14 @@
 import { auth } from './Firebase';
 
 function Message({ message }) {
+    const timestamp = new Date(message.createdAt.seconds * 1000);
     return (
         // Determine whether the message was sent or recieved by checking the author and current user
         <div className={`message ${ auth.currentUser.uid === message.uid ? 'sent' : 'received' }`}>
             {/* Generate profile picture based on the photoURL attached with the message */}
-            <img src={message.photoURL} alt={`Profile of ${message.displayName}`}></img>
+            <img src={message.photoURL} alt={`Profile of ${message.displayName}`} referrerpolicy="no-referrer"/><img />
+            <p>{message.displayName}</p>
+            <p>{timestamp.getDate() + '/' + timestamp.getMonth() + '/' + timestamp.getFullYear() + ' at ' + timestamp.getHours() + ':' + timestamp.getMinutes() }</p>
             <p>{message.text}</p>
         </div>
     );
