0627b1c1a517d46b4b3d4ee3a8d34017a54f52d1
Lucas Bubner <lkbubner@proton.me>
2023-01-04 19:01:30 +1030

    Fixed application bugs and formatting

--- src/Message.js
+++ src/Message.js
@@ -1,14 +1,30 @@
 import { auth } from './Firebase';
 
+const addZero = (value) => {
+    return value < 10 ? '0' + value : value;
+}
+
 function Message({ message }) {
-    const timestamp = new Date(message.createdAt.seconds * 1000);
+    let timestamp;
+    try {
+        // Attempt to retrieve a timestamp from the server
+        timestamp = new Date(message.createdAt.seconds * 1000);
+    } catch (error) {
+        // If it fails, this must mean the message is being sent, and we can use the current Unix time
+        timestamp = new Date(Date.now());
+    }
+
     return (
         // Determine whether the message was sent or recieved by checking the author and current user
-        <div className={`message ${ auth.currentUser.uid === message.uid ? 'sent' : 'received' }`}>
+        <div className={`message ${auth.currentUser.uid === message.uid ? 'sent' : 'received'}`}>
             {/* Generate profile picture based on the photoURL attached with the message */}
-            <img src={message.photoURL} alt={`Profile of ${message.displayName}`} referrerpolicy="no-referrer"/><img />
+            <img src={message.photoURL} alt={`Profile of ${message.displayName}`} referrerPolicy="no-referrer" />
+
             <p>{message.displayName}</p>
-            <p>{timestamp.getDate() + '/' + timestamp.getMonth() + '/' + timestamp.getFullYear() + ' at ' + timestamp.getHours() + ':' + timestamp.getMinutes() }</p>
+
+            {/* Display the proper formatted date and time metadata with each message */}
+            <p>{addZero(timestamp.getDate()) + '/' + (addZero(timestamp.getMonth() + 1)) + '/' + timestamp.getFullYear() + ' at ' + addZero(timestamp.getHours()) + ':' + addZero(timestamp.getMinutes())}</p>
+
             <p>{message.text}</p>
         </div>
     );
