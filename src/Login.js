f7ff66b6ce7235712a3593681ec64743aa589d49
Lachlan Paul <lachlanpaul03@gmail.com>
2023-01-06 21:52:16 +1030

    Did some stuff, pretty minor changes

--- src/Login.js
+++ src/Login.js
@@ -3,15 +3,18 @@
  *    Will be shown to the user if userdata is not present upon visiting.
  */
 import { signInWithGoogle } from './Firebase';
+import './login.css'
 
 function Login() {
     return (
-        <div className="login">
+       <center>
+       <div className="login" class="center">
             <h2>Bunyips Chatapp</h2>
-            <button onClick={signInWithGoogle}>
+            <button onClick={signInWithGoogle} className="btn">
                 Sign in with Google
             </button>
         </div>
+        </center>
     );
 }
 
