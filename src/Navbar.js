c9fb2b21fe2c4ccb4113ae24568727c3c87b3ce7
Lachlan Paul <lachlanpaul03@gmail.com>
2023-01-11 21:17:19 +1030

    Started work on options menu Improved Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,28 +9,23 @@ import { auth } from "./Firebase";
 // import {message} from "./Message";
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <img
-                className="navbar-brand"
-                src={auth.currentUser.photoURL}
-                referrerPolicy="no-referrer"
-                alt={`Profile of ${auth.currentUser.displayName}`}
-            />
-            <p className="navbar-name">
-                Signed in as {auth.currentUser.displayName}
-            </p>
-            <button
-                className="sobtn"
-                onClick={async () => await auth.signOut()}
-            >
-                Sign out
-            </button>
-            <button className="joke" onClick={async () => alert("lol no")}>
-                Enable Light Mode
-            </button>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <img
+        className="navbar-brand"
+        src={auth.currentUser.photoURL}
+        referrerPolicy="no-referrer"
+        alt={`Profile of ${auth.currentUser.displayName}`}
+      />
+      <p className="navbar-name">Signed in as {auth.currentUser.displayName}</p>
+      <button className="sobtn" onClick={async () => await auth.signOut()}>
+        Sign out
+      </button>
+      <button className="joke" onClick={async () => alert("lol no")}>
+        Enable Light Mode
+      </button>
+    </div>
+  );
 }
 
 export default Navbar;
