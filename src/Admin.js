4af6aff6bfdec7bd1ba714f1523126b2f39658cb
Lucas Bubner <lkbubner@proton.me>
2023-01-11 21:03:17 +1030

    Finished Admin module

--- src/Admin.js
+++ src/Admin.js
@@ -4,12 +4,20 @@
  */
 import { useState, useEffect } from "react";
 import { auth, db } from "./Firebase";
-import { collection, onSnapshot, deleteDoc, doc } from "firebase/firestore";
+import {
+    collection,
+    onSnapshot,
+    deleteDoc,
+    doc,
+    setDoc,
+} from "firebase/firestore";
 import "./Admin.css";
 
 function Admin() {
     const [isAdminAuthorised, setisAdminAuthorised] = useState();
 
+    // Ensure that a user's email using this module is on the admin collection
+    // The admin collection can only be altered by the Firebase owner.
     useEffect(() => {
         const unsubscribe = onSnapshot(collection(db, "admin"), (doc) => {
             let isAuthorised = false;
@@ -24,13 +32,13 @@ function Admin() {
                 setisAdminAuthorised(false);
             }
         });
-    
+
         return () => {
             unsubscribe();
-        }
-    
+        };
     }, []);
 
+    // Delete the read permissions from an email address
     async function removeRead(email) {
         if (!window.confirm("Remove read permission from " + email + "?"))
             return;
@@ -39,10 +47,32 @@ function Admin() {
             .then(() => alert("Operation completed."));
     }
 
+    // Delete the write permissions from an email address
     async function removeWrite(email) {
         if (!window.confirm("Remove write permission from " + email + "?"))
             return;
         await deleteDoc(doc(db, "write", email))
+            .catch((error) => alert(error))
+            .then(() => alert("Operation completed."));
+    }
+
+    // Add read permissions to an email address by making a new doc with the email's name
+    // No data is placed inside this doc, but is used for security purposes
+    async function addRead() {
+        const email = prompt(
+            "Enter an email address to grant application access to."
+        );
+        await setDoc(doc(db, "read", email), {})
+            .catch((error) => alert(error))
+            .then(() => alert("Operation completed."));
+    }
+
+    // Add write permissions to an email address by making a new doc with the email's name
+    async function addWrite() {
+        const email = prompt(
+            "Enter an email address to grant message send access to."
+        );
+        await setDoc(doc(db, "write", email), {})
             .catch((error) => alert(error))
             .then(() => alert("Operation completed."));
     }
@@ -53,21 +83,27 @@ function Admin() {
     // There was a memory leak here that leaked hundreds of megabytes per second with these two listeners.
     // Thanks ChatGPT for fixing it...
     useEffect(() => {
-        const readListener = onSnapshot(collection(db, "read"), (querySnapshot) => {
-            const readArray = [];
-            querySnapshot.forEach((doc) => {
-                readArray.push(doc.id);
-            });
-            setReadDocs(readArray);
-        });
+        const readListener = onSnapshot(
+            collection(db, "read"),
+            (querySnapshot) => {
+                const readArray = [];
+                querySnapshot.forEach((doc) => {
+                    readArray.push(doc.id);
+                });
+                setReadDocs(readArray);
+            }
+        );
 
-        const writeListener = onSnapshot(collection(db, "write"), (querySnapshot) => {
-            const writeArray = [];
-            querySnapshot.forEach((doc) => {
-                writeArray.push(doc.id);
-            });
-            setWriteDocs(writeArray);
-        });
+        const writeListener = onSnapshot(
+            collection(db, "write"),
+            (querySnapshot) => {
+                const writeArray = [];
+                querySnapshot.forEach((doc) => {
+                    writeArray.push(doc.id);
+                });
+                setWriteDocs(writeArray);
+            }
+        );
 
         return () => {
             readListener();
@@ -82,36 +118,40 @@ function Admin() {
                     <div className="read">
                         <p>READ</p>
                         <ul>
-                            {readDocs.map((doc, index) => {
+                            {readDocs.map((doc) => {
                                 return (
                                     <li>
                                         <button
                                             onClick={() => removeRead(doc)}
-                                            key={index}>
+                                            key={doc.id}>
                                             {doc}
                                         </button>
                                     </li>
                                 );
                             })}
                         </ul>
-                        <button className="new">Add a new user</button>
+                        <button onClick={() => addRead()} className="new">
+                            Add a new user
+                        </button>
                     </div>
                     <div className="write">
                         <p>WRITE</p>
                         <ul>
-                            {writeDocs.map((doc, index) => {
+                            {writeDocs.map((doc) => {
                                 return (
                                     <li>
                                         <button
                                             onClick={() => removeWrite(doc)}
-                                            key={index}>
+                                            key={doc.id}>
                                             {doc}
                                         </button>
                                     </li>
                                 );
                             })}
                         </ul>
-                        <button className="new">Add a new writer</button>
+                        <button onClick={() => addWrite()} className="new">
+                            Add a new writer
+                        </button>
                     </div>
                 </div>
             ) : (
