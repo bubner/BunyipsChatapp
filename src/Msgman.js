d92594814ae05aa2e2699670ffc03059d07a5772
Lucas Bubner <lkbubner@proton.me>
2023-01-17 18:25:00 +1030

    Started Msgman module

---
+++ src/Msgman.js
@@ -0,0 +1,11 @@
+/**
+ *    Moderative module to allow users to modify current messages in Firestore, depending on their permission nodes.
+ *    @author Lucas Bubner, 2023
+ */
+import "./Msgman.css";
+
+function Msgman() {
+    return;
+}
+
+export default Msgman;
