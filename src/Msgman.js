74f546134cb4d888a87654f183cd671e0b4533a6
Lucas Bubner <lkbubner@proton.me>
2023-01-18 20:39:45 +1030

    deleteMsg now deletes from Firebase storage if a file is being stored there as well

--- src/Msgman.js
+++ src/Msgman.js
@@ -11,23 +11,23 @@ import {
     doc,
     getDoc,
 } from "firebase/firestore";
-import { db, auth } from "./Firebase";
+import { db, auth, storage } from "./Firebase";
+import { ref, deleteObject } from "firebase/storage";
+import { getFileURL } from "./Message";
 import Popup from "reactjs-popup";
 
 function Msgman({ id }) {
     // Delete a message from Firestore based on the message id.
     // The message ID is stored as the same as the document ID.
-    function deleteMsg() {
+    async function deleteMsg() {
         if (!window.confirm("Delete message: " + id + "?")) return;
-        onSnapshot(collection(db, "messages"), (snapshot) => {
-            snapshot.forEach((message) => {
-                if (message.id === id) {
-                    deleteDoc(doc(db, "messages", message.id)).catch((err) =>
-                        alert(err)
-                    );
-                }
-            });
-        });
+        const document = await getDoc(doc(db, "messages", id));
+        if (!document.data().isMsg) {
+            // Check if the document contains a file, if so, we'll have to delete from Firebase storage too
+            const fileRef = ref(storage, getFileURL(document.data().text));
+            await deleteObject(fileRef).catch((err) => alert(err));
+        }
+        deleteDoc(doc(db, "messages", id)).catch((err) => alert(err));
     }
 
     // Using same admin authorisation from Admin.js
@@ -55,28 +55,17 @@ function Msgman({ id }) {
     // Determine whether a user should have moderation over their own message
     const [isAuthorised, setIsAuthorised] = useState(false);
     useEffect(() => {
-        const unsubscribe = onSnapshot(collection(db, "messages"), (doc) => {
-            let canModerate = false;
-            doc.forEach((doc) => {
-                if (auth.currentUser.uid === doc.data().uid && doc.id === id) {
-                    canModerate = true;
-                }
-            });
-            if (canModerate) {
-                setIsAuthorised(true);
-            } else {
-                setIsAuthorised(false);
-            }
-        });
-
-        return () => {
-            unsubscribe();
-        };
+        const doctarget = doc(db, "messages", id);
+        if (doctarget.uid === auth.currentUser.uid) {
+            setIsAuthorised(true);
+        } else {
+            setIsAuthorised(false);
+        }
     }, [id]);
 
     // Function to show the metadata of a certain message
     async function viewData() {
-        const message = await getDoc(doc(collection(db, "messages"), id));
+        const message = await getDoc(doc(db, "messages", id));
         const msgData = message.data();
         alert(
             `Message author: ${msgData.displayName}\nAuthor UID: ${msgData.uid}\nMessage ID: ${msgData.id.id}\nMessage creation time: ${msgData.createdAt.seconds}\nIs a text message? ${msgData.isMsg}\nMessage content: "${msgData.text}"`
@@ -94,7 +83,7 @@ function Msgman({ id }) {
                         </p>
                         <hr />
                         <button onClick={() => viewData()}>
-                            View metadata
+                            View message metadata
                         </button>
                         <hr />
                         {(isAdminAuthorised || isAuthorised) && (
